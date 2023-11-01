457838d2d3e214974553c7ec5e70fa19ca801568
Manuel Perez <Manuel.Perez2@thomsonreuters.com>
2023-11-01 16:33:02 -0400

    Filtro de Categorias y titulo

--- src/screens/Category.jsx
+++ src/screens/Category.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
 import MovieCard from "../components/MovieCard";
 import { useParams } from "react-router-dom";
+import Filter from "../components/Filter";
 
 function Category (){
     const [movies, setMovies] = useState([]);
     const [favorites, setFavorites] = useState([]);
+    const [category, setCategory] = useState('');
+    const [inputValue, setInputValue] = useState('');
     const { with_genres } = useParams();
 
     useEffect(() =>{
@@ -20,6 +23,8 @@ function Category (){
         .then(response => response.json())
         .then(response => setMovies(response.results))
         .catch(err => console.error(err));
+
+        setInputValue('');
     }, [with_genres]);
 
     useEffect(() => {
@@ -27,10 +32,29 @@ function Category (){
         storedFavorites && setFavorites(storedFavorites);
     }, []);
 
+    useEffect(()=>{
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OGYyYTYyZDFhNjRmZmI1ODliMzhlMWRmZjhhZGYzNCIsInN1YiI6IjY1Mzc5ZTAyYWUzNjY4MDBlYTljNGFkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sfy6iZZoRv-xLXx0J3o4kPSotyZJ0vi0Z5GeaxW3Co8'
+            }
+          };
+          
+          fetch('https://api.themoviedb.org/3/genre/movie/list?language=es-AR', options)
+            .then(response => response.json())
+            .then(response => setCategory(response.genres.filter(x => x.id == with_genres)[0].name))
+            .catch(err => console.error(err));
+    });
+
+    const filteredMovies = movies.filter((movie) => movie.title.toLowerCase().includes(inputValue.toLowerCase()))
+
     return <section>
+        <h1 className="text-center text-primary">{category}</h1>
+    <Filter inputValue={inputValue} setInputValue={setInputValue} />
     <ul className="flex justify-center items-center flex-wrap gap-6">
-    {movies.length > 0 ? (
-        movies.slice(0,9).map(movie=><MovieCard key={movie.id} movie={movie} favorites={favorites} setFavorites={setFavorites} />)
+    {filteredMovies.length > 0 ? (
+        filteredMovies.map(movie=><MovieCard key={movie.id} movie={movie} favorites={favorites} setFavorites={setFavorites} />)
     ) : (<p>Cargando...</p>)
     }
     </ul>
