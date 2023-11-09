504eefb57fbb6d2cd4034f72621270447f648ca7
ManuelFran <139390394+ManuelFran@users.noreply.github.com>
2023-11-09 07:52:37 -0400

    cambios de ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import  { BrowserRouter, Routes, Route } from 'react-router-dom'
-import '/App.css'
+import './App.css'
 import Home from '/screens/Home'
 import Category from '/screens/Category'
 import Favorites from '/screens/Favorites'
